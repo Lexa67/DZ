326cf425737da2d5dbff95b4a42f96db35bf703f
Lexa <3aj4ik504@gmail.com>
2023-04-08 00:31:17 +0300

    comment

--- sasha.rb
+++ sasha.rb
@@ -1,5 +1,5 @@
 i = 0
-
+point = 0
 loop  do
 	i = i + 1
 	a = rand(1..10)
@@ -10,39 +10,33 @@ loop  do
 	puts "Сколько будет #{a} * #{b}?"
 	puts "Введите свой ответ..."
 
-sasha = nil
+o = 0
 
-		loop do
-			sasha = gets.to_i
-				if sasha.between?(1,100)
-				 	puts "Ваш ответ #{sasha}"
-				 	break
-				else
-				 	puts "Некорректный ввод. Введите цыфру от 1 до 100"
-				end		
+loop do
+	
+	sasha = gets.to_i
+	
+	if sasha == c
+		puts "Верно!"
+		point = point + 1
+		break
+	
+	elsif sasha.between?(1,100) == false
+		puts "Введите число от 1 до 100"
+	
+	else
+		o = o + 1
+		puts "Неверно!"
+		puts "Попробуй еще..."
+		if o == 2
+			puts " Верный ответ #{c}"
+			point = point - 1
 		end
-
-		loop do
-
-			if sasha == c
-				puts "Верно!"
-				break
-			else
-				o = 0
-				loop do 
-				o = o + 1
-				puts "Неверно!"
-				puts "Попробуй еще..."
-				sasha = gets.to_i
-					if o == 4 || sasha == c
-						puts " Верный ответ #{c}"
-						break
-					end
-				end
-			end
-		end
+	end
+end
 	if i == 20
 		puts "Молодец!"
+		puts "Правильных ответов #{point}"
 		break
 	end
 end
