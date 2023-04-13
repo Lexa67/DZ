f39cd75c64084db0a597125a0029ab389a15a727
Lexa <3aj4ik504@gmail.com>
2023-04-13 18:46:17 +0300

    13.04.2023

--- Viselitsa/viselitsa.rb
+++ Viselitsa/viselitsa.rb
@@ -1,30 +1,16 @@
-require_relative 'methods'
+require_relative 'game'
+require_relative 'result_printer'
 
-cls
+printer = ResultPrinter.new
 
-puts "Игра виселица. Версия 1"
+slovo = ARGV[0]
 
-letters = get_letters
+game = Game.new(slovo)
 
-errors = 0 
+while game.status == 0 do
+	printer.print_status(game)
 
-bad_letters = []
-good_letters = []
-
-while errors < 7 do
-	print_status(letters, good_letters,bad_letters, errors)
-
-	puts "Введите букву:"
-
-	user_input = get_user_input
-
-	result = check_input(user_input, letters, good_letters, bad_letters)
-
-	if (result == -1)
-		errors += 1
-	elsif (result == 1)
-		break
-	end
+	game.ask_next_letter	
 end
 
-print_status(letters, good_letters, bad_letters, errors)
+printer.print_status(game)
