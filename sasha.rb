059330c9352c63a5bd2d76881ddf6678cb15a877
Lexa67 <3aj4ik504@gmail.com>
2023-05-09 18:54:19 +0300

    09.05.2023_17:30

--- sasha.rb
+++ sasha.rb
@@ -1,13 +1,13 @@
 puts "Сколько примеров ты хочешь решить?"
 user = gets.to_i
-i = 0
+popitka = 0
 point = 0
 loop  do
-	i = i + 1
+	popitka = popitka + 1
 	a = rand(1..10)
 	b = rand(1..10)
 
-	c = a * b
+	result = a * b
 
 	puts "Сколько будет #{a} * #{b}?"
 	puts "Введите свой ответ..."
@@ -15,28 +15,28 @@ loop  do
 o = 0
 
 loop do
-	
+
 	sasha = gets.to_i
-	
-	if sasha == c
+
+	if sasha == result
 		puts "Верно!"
 		point = point + 1
 		break
-	
+
 	elsif sasha.between?(1,100) == false
 		puts "Введите число от 1 до 100"
-	
+
 	else
 		o = o + 1
 		puts "Неверно!"
 		puts "Попробуй еще..."
 		if o == 2
-			puts " Верный ответ #{c}"
+			puts " Верный ответ #{result}"
 			point = point - 1
 		end
 	end
 end
-	if i == user
+	if popitka == user
 		puts "Молодец!"
 		puts "Правильных ответов #{point} из #{user}"
 		break
